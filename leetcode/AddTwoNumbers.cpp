77b37d17d75bd54aa9f0fa5a24ce640a014c5d2b
blink <blinking1001@gmail.com>
2020-10-31 13:54:58 +0900

    (EDIT) LeetCode #2 Add Two Numbers 인덴트 수정
    LeetCode 두번째 문제 코드 인덴트 수정함.

--- leetcode/AddTwoNumbers.cpp
+++ leetcode/AddTwoNumbers.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-       int sum = 0;
-       int carry = 0;
-       ListNode* result = nullptr;
-       ListNode* nextNode = nullptr;
-       while ((l1 != nullptr || l2 != nullptr) || carry > 0 ) {
-       	int num1 = 0, num2 = 0;
-       	if (l1 != nullptr) {
-       		num1 = l1->val;
-       		l1 = l1->next;
-       	}
-       	
-       	if (l2 != nullptr) {
-       		num2 = l2->val;
-       		l2 = l2->next;
-       	}
-       	
-       	sum = (num1 + num2 + carry) % 10;
-       	carry = (num1 + num2 + carry >= 10) ? 1 : 0;
-       	
-       	if (result == nullptr) {
-	        	result = new ListNode(sum);
-	        	nextNode = result;
-	        }
-	        else {
-				nextNode->next = new ListNode(sum);
-				nextNode = nextNode->next;
-	        }
-       }
-       return result;
-   }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int sum = 0;
+        int carry = 0;
+        ListNode* result = nullptr;
+        ListNode* nextNode = nullptr;
+        while ((l1 != nullptr || l2 != nullptr) || carry > 0 ) {
+            int num1 = 0, num2 = 0;
+            if (l1 != nullptr) {
+                num1 = l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                num2 = l2->val;
+                l2 = l2->next;
+            }
+
+            sum = (num1 + num2 + carry) % 10;
+            carry = (num1 + num2 + carry >= 10) ? 1 : 0;
+
+            if (result == nullptr) {
+                result = new ListNode(sum);
+                nextNode = result;
+            }
+            else {
+                nextNode->next = new ListNode(sum);
+                nextNode = nextNode->next;
+            }
+        }
+        return result;
+    }
 };
